364fec91e16404efe5a4acd36df920185fc6583a
GlebIgnatev21 <Konnard21@yandex.ru>
2022-07-13 15:50:18 +0300

    Решены три задачи.

---
+++ src/HomeWork_Lesson1/Hard.java
@@ -0,0 +1,4 @@
+package HomeWork_Lesson1;
+
+public class Hard {
+}
---
+++ src/HomeWork_Lesson1/Simpl_HomeWork_Lesson1.java
@@ -0,0 +1,67 @@
+package HomeWork_Lesson1;
+
+public class Simpl_HomeWork_Lesson1 {
+
+    public static void main(String[] args) {
+        //Базовый уровень
+        //Задача №1
+        //Дано (переменные ниже менять нельзя)
+        String hi = "                Hello ";
+//        System.out.println(hi.length());//высчитать пробелы, забыл убрать.
+        String world = " WoRld!";
+        char newLine = '\n';
+        //Создать из трех переменных единую строку,
+        //Привести к правильному виду (Ниже) используя String.trim()
+        //затроить (Можно вызвать тольку одну команду System.out.println())
+        //
+        //Результат вывода на экран:
+        //Hello world!
+        //Hello world!
+        //Hello world!
+
+        hi = hi.trim().replaceAll("H", "//H");
+//        world = world.trim();//Различные варианты
+        world = world.trim().replaceAll("WoR", "wor");
+//        world = world.trim().replaceAll("W", "w");
+        String resaltHiWorldNewLine = hi + ' ' + world + newLine;
+        resaltHiWorldNewLine = resaltHiWorldNewLine.repeat(3);
+//        resaltHiWorldNewLine = resaltHiWorldNewLine.replaceAll("Hello WoRld!", "Hello world!").repeat(3);
+
+        //Если из условия убрать trim,  то можно так решить.
+//        String resaltHiWorldNewLine = hi + world + newLine;
+//        resaltHiWorldNewLine = resaltHiWorldNewLine.replaceAll("                Hello  WoRld!", "Hello world!").repeat(3);
+        //P.S. //стоят так, типа я буквально понял Результат вывода на экран:
+
+        System.out.println(resaltHiWorldNewLine);
+
+
+
+        //Задача №2
+        //Создать переменные с ростом (!в метрах), весом в кило.
+        //Произвести расчет индекса массы тела (вес) / (рост * рост) используя переменные, вывести на экран
+        //Пример результата вывода на экран:
+        //21.0345645
+
+        int wightKg = 98;
+        double growthMetr = 1.82;
+        double resultBodiMassIndex = wightKg/(growthMetr*growthMetr);
+        System.out.println(resultBodiMassIndex);
+
+
+
+        //Задача №3
+        //Создать из массива букв a,b,c,d,e, строку,вывести на экран , поменять в массиве 4 букву по счету на h,
+        //и снова создать строку, вывести на экран
+        //Ожидаемый результат:
+        //abcde
+        //abche
+
+        char[] arreyLetters = new char[]{'a', 'b', 'c', 'd', 'e'};
+        System.out.println(arreyLetters);
+        arreyLetters[3] = 'h';
+        System.out.println(arreyLetters);
+
+
+
+    }
+}
